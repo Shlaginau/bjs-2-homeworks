77d5024a273c3c33a9e02ca5884c37d2ed5ec4e1
Uliana Shlagina <shlaginau@gmail.com>
2023-03-24 11:01:19 +0700

    Внесены правки после проверки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,22 +5,17 @@ function solveEquation(a, b, c) {
   let x1;
   let x2;
   let d = b**2 - 4 * a * c;
-  console.log(d);
   
-  if (d < 0) {
-    return arr;
-    
-  } else if (d === 0) {
-      x1 = - b / (2 * a);
-      arr.push(x1);
-      return arr;
-     
+  if (d === 0) {
+    x1 = - b / (2 * a);
+    arr.push(x1);
+       
   } else if (d > 0) {
       x1 = (- b + Math.sqrt(d)) / (2 * a);
       x2 = (- b - Math.sqrt(d)) / (2 * a);
       arr.push(x1,x2);
-      return arr;
   }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
