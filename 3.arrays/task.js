142e01de37bcaf0e017cde21348a0d9f7bb39224
Uliana Shlagina <shlaginau@gmail.com>
2023-03-29 16:55:37 +0700

    3.arrays done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+ return arr1.every((element, index) => arr1.length === arr2.length && element === arr2[index]); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users.
+    filter(user => user.gender === gender).
+    map(user => user.age).
+    reduce((average, userAge, index, array) => average + userAge / array.length, 0);
 }
+
+
+/* мой вариант 
+
+function getUsersNamesInAgeRange(users, gender) {
+  return users.filter(user => user.gender === gender && user.age !== 0).map(user => user.age).reduce((acc, age, index, people) => {
+      acc+=age;
+      if (index === people.length - 1) {
+          return acc / people.length;
+          }
+      return acc;
+      },0)
+    }
+*/
